e9bba1ce260ebad4152e746e3e6bcc63d7a14423
murhussain <husseinmurashi@yahoo.com>
2022-09-19 19:30:47 +0200

    getServerSideProp on filtering page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 
-import { getEventById,getAllEvents } from '../../helper/api-util';
+import { getEventById,getFeaturedEvents } from '../../helper/api-util';
 
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
@@ -12,9 +12,9 @@ function EventDetailPage(props) {
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading ....</p>
+      </div>
     );
   }
 
@@ -43,17 +43,19 @@ export async function getStaticProps(context) {
   return {
     props: {
       selectedEvent: event
-    }
+    },
+    revalidate: 30
   }
 }
 
 export async function getStaticPaths(){
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
 
   const paths = events.map(event => ({params: {eventId: event.id}}));
 
   return {
     paths: paths,
-    fallback: false
+    fallback: true
+    // fallback: 'blocking' removing loading msg
   };
 }
